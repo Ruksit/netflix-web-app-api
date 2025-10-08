cfe44216e496f390a5e2789c8ce7c55861266dc3
Ruksit <ruksitm@gmail.com>
2025-10-08 12:29:02 +0700

    chore: enable CORS for Next.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MoviesModule } from './movies/movies.module';
+import { MoviesController } from './presentation/controllers/movies/movies.controller';
+import { TmdbMovie } from './data/tmdb-adapter/tmdb-movie/tmdb-movie';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), MoviesModule],
+  controllers: [AppController, MoviesController],
+  providers: [AppService, TmdbMovie],
 })
 export class AppModule {}
