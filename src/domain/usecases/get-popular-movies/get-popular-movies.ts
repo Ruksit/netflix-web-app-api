337e5b9624b4d532025ae2887e18c34be7d02180
Ruksit <ruksitm@gmail.com>
2025-10-08 12:54:23 +0700

    feat(domain): add GetPopularMoviesUseCase to fetch popular movies

--- src/domain/usecases/get-popular-movies/get-popular-movies.ts
+++ src/domain/usecases/get-popular-movies/get-popular-movies.ts
@@ -1,1 +1,17 @@
-export class GetPopularMovies {}
+import { Inject, Injectable } from '@nestjs/common';
+import {
+  MOVIE_REPOSITORY,
+  MovieRepository,
+} from '../../../data/repositories/movie/movie.interface';
+import { MovieEntity } from '../../entities/movie/movie.entity';
+
+@Injectable()
+export class GetPopularMoviesUseCase {
+  constructor(
+    @Inject(MOVIE_REPOSITORY) private readonly repo: MovieRepository,
+  ) {}
+
+  execute(page = 1): Promise<MovieEntity[]> {
+    return this.repo.getPopular(page);
+  }
+}
