fee035de78c03e0508ae1f8624e9780f25d18e42
Ruksit <ruksitm@gmail.com>
2025-10-08 12:47:21 +0700

    feat(repository): define MovieRepository interface and getPopular method

--- src/data/repositories/movie/movie.interface.ts
+++ src/data/repositories/movie/movie.interface.ts
@@ -1,1 +1,7 @@
-export interface Movie {}
+import { MovieEntity } from '../../../domain/entities/movie/movie.entity';
+
+export interface MovieRepository {
+  getPopular(page?: number): Promise<MovieEntity[]>;
+}
+
+export const MOVIE_REPOSITORY = 'MovieRepository';
