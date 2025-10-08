81ab512f71a3f0ddba1401429b78d3dc8da31caa
Ruksit <ruksitm@gmail.com>
2025-10-08 13:14:52 +0700

    fix(app): remove stray controller/provider from AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MoviesModule } from './movies/movies.module';
-import { MoviesController } from './presentation/controllers/movies/movies.controller';
-import { TmdbMovie } from './data/tmdb-adapter/tmdb-movie/tmdb-movie';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), MoviesModule],
-  controllers: [AppController, MoviesController],
-  providers: [AppService, TmdbMovie],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
